b4d3fbb0535074f2153b2b9bad53fdf654ddedd1
Bryan Silverthorn <bcs@cargo-cult.org>
2010-08-13 14:41:12 -0500

    Clean up the Bellman benchmarking code.

--- src/python/borg/tools/bench_bellman.py
+++ src/python/borg/tools/bench_bellman.py
@@ -12,24 +12,12 @@ def main():
     Benchmark the Bellman plan computation code.
     """
 
-    # need a place to dump profiling results
-    from tempfile import NamedTemporaryFile
+    from borg.portfolio.bellman           import compute_bellman_plan
+    from borg.portfolio.test.test_bellman import build_real_model
 
-    with NamedTemporaryFile() as named:
-        # profile the computation
-        from cProfile                         import runctx
-        from borg.portfolio.bellman           import compute_bellman_plan
-        from borg.portfolio.test.test_bellman import build_real_model
+    model = build_real_model()
+    stats = call_profiled(lambda: compute_bellman_plan(model, 6, 1e6, 1.0))
 
-        context = {"c": compute_bellman_plan, "m": build_real_model()}
-        profile = runctx("c(m, 6, 1e6, 1.0)", {}, context, named.name)
-
-        # extract the results
-        from pstats import Stats
-
-        stats = Stats(named.name)
-
-    # display a report
     stats.strip_dirs()
     stats.sort_stats("cumulative")
     stats.print_stats()
