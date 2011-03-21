1b3f0b6805a9f6f62b1ec579fa614b58a113983a
Bryan Silverthorn <bcs@cargo-cult.org>
2011-03-21 15:07:10 -0500

    Add support for the SCIP solver(s).

--- src/python/borg/domains/pb/solvers.py
+++ src/python/borg/domains/pb/solvers.py
@@ -2,6 +2,7 @@
 
 import re
 import itertools
+import numpy
 import cargo
 import borg
 
@@ -13,35 +14,38 @@ def basic_command(relative):
     return ["{{root}}/{0}".format(relative), "{task}", "{seed}"]
 
 nlc_commands = {
-    "pbct-0.1.2-linear": ["{root}/pbct-0.1.2-linux32", "--model", "{task}"],
-    "sat4j-pb-v20101225": [
-        "java",
-        "-server",
-        "-jar",
-        "{root}/sat4j-pb-v20101225/sat4j-pb.jar",
-        "{task}",
-        ],
-    "sat4j-pb-v20101225-cutting": [
-        "java",
-        "-server",
-        "-jar",
-        "{root}/sat4j-pb-v20101225/sat4j-pb.jar",
-        "CuttingPlanes",
-        "{task}",
-        ],
+    #"pbct-0.1.2-linear": ["{root}/pbct-0.1.2-linux32", "--model", "{task}"],
+    #"sat4j-pb-v20101225": [
+        #"java",
+        #"-server",
+        #"-jar",
+        #"{root}/sat4j-pb-v20101225/sat4j-pb.jar",
+        #"{task}",
+        #],
+    #"sat4j-pb-v20101225-cutting": [
+        #"java",
+        #"-server",
+        #"-jar",
+        #"{root}/sat4j-pb-v20101225/sat4j-pb.jar",
+        #"CuttingPlanes",
+        #"{task}",
+        #],
     }
 lin_commands = {
-    "bsolo_pb10-l1": ["{root}/bsolo_pb10", "-t1000000", "-m2048", "-l1", "{task}"],
-    "bsolo_pb10-l2": ["{root}/bsolo_pb10", "-t1000000", "-m2048", "-l2", "{task}"],
-    "bsolo_pb10-l3": ["{root}/bsolo_pb10", "-t1000000", "-m2048", "-l3", "{task}"],
-    "wbo1.4a": ["{root}/wbo1.4a", "-time-limit=1000000", "-file-format=opb", "{task}"],
-    "wbo1.4b-fixed": ["{root}/wbo1.4b-fixed", "-time-limit=1000000", "-file-format=opb", "{task}"],
-    "clasp-1.3.7": ["{root}/clasp-1.3.7/clasp-1.3.7-x86-linux", "--seed={seed}", "{task}"],
+    #"bsolo_pb10-l1": ["{root}/bsolo_pb10", "-t1000000", "-m2048", "-l1", "{task}"],
+    #"bsolo_pb10-l2": ["{root}/bsolo_pb10", "-t1000000", "-m2048", "-l2", "{task}"],
+    #"bsolo_pb10-l3": ["{root}/bsolo_pb10", "-t1000000", "-m2048", "-l3", "{task}"],
+    #"wbo1.4a": ["{root}/wbo1.4a", "-time-limit=1000000", "-file-format=opb", "{task}"],
+    #"wbo1.4b-fixed": ["{root}/wbo1.4b-fixed", "-time-limit=1000000", "-file-format=opb", "{task}"],
+    #"clasp-1.3.7": ["{root}/clasp-1.3.7/clasp-1.3.7-x86-linux", "--seed={seed}", "{task}"],
     }
-commands = dict(nlc_commands.items() + lin_commands.items())
+scip_commands = {
+    "scip-2.0.1-clp": ["{root}/scip-2.0.1.linux.x86_64.gnu.opt.clp", "-f", "{task}"],
+    "scip-2.0.1-spx": ["{root}/scip-2.0.1.linux.x86_64.gnu.opt.spx", "-f", "{task}"],
+    }
 
-def parse_pb_output(stdout):
-    """Parse a solver's standard competition-format output."""
+def parse_competition(stdout):
+    """Parse output from a standard competition solver."""
 
     match = re.search(r"^s +([a-zA-Z ]+) *\r?$", stdout, re.M)
 
@@ -66,6 +70,32 @@ def parse_pb_output(stdout):
 
     return None
 
+def parse_scip(variables, optimization, stdout):
+    """Parse output from the SCIP solver(s)."""
+
+    answer_match = re.search(r"^SCIP Status *: *problem is solved \[([a-zA-Z ]+)\] *\r?$", stdout, re.M)
+
+    if answer_match:
+        (status,) = answer_match.groups()
+        solution = numpy.zeros(variables, bool)
+
+        if status in ("optimal solution found"):
+            answer_type = "OPTIMUM FOUND" if optimization else "SATISFIABLE"
+            trues = map(int, re.findall(r"^x([0-9]+) *1[ \t]*\(obj:.+\) *\r?$", stdout, re.M))
+
+            solution[trues] = True
+
+            certificate = [("" if t else "-") + ("x%i" % v) for (v, t) in enumerate(solution)]
+        elif status == "infeasible":
+            answer_type = "UNSATISFIABLE"
+            certificate = None
+        else:
+            return None
+
+        return (answer_type, certificate)
+
+    return None
+
 class PseudoBooleanSolverFactory(object):
     def __init__(self, command):
         self._command = command
@@ -73,7 +103,7 @@ class PseudoBooleanSolverFactory(object):
     def __call__(self, task, stm_queue = None, solver_id = None):
         return \
             borg.solver_io.RunningSolver(
-                parse_pb_output,
+                parse_competition,
                 self._command,
                 task.path,
                 stm_queue = stm_queue,
@@ -84,14 +114,31 @@ class LinearPseudoBooleanSolverFactory(PseudoBooleanSolverFactory):
     def __call__(self, task, stm_queue = None, solver_id = None):
         return \
             borg.solver_io.RunningSolver(
-                parse_pb_output,
+                parse_competition,
                 self._command,
                 task.get_linearized_path(),
                 stm_queue = stm_queue,
                 solver_id = solver_id,
                 )
 
+class SCIP_SolverFactory(object):
+    def __init__(self, command):
+        self._command = command
+
+    def __call__(self, task, stm_queue = None, solver_id = None):
+        parse = cargo.curry(parse_scip, task.opb.N, task.opb.objective is not None)
+
+        return \
+            borg.solver_io.RunningSolver(
+                parse,
+                self._command,
+                task.path,
+                stm_queue = stm_queue,
+                solver_id = solver_id,
+                )
+
 nlc_named = dict(zip(nlc_commands, map(PseudoBooleanSolverFactory, nlc_commands.values())))
 lin_named = dict(zip(lin_commands, map(LinearPseudoBooleanSolverFactory, lin_commands.values())))
-named = dict(nlc_named.items() + lin_named.items())
+scip_named = dict(zip(scip_commands, map(SCIP_SolverFactory, scip_commands.values())))
+named = dict(nlc_named.items() + lin_named.items() + scip_named.items())
 
