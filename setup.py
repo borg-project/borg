f6b009c264c09108a7f102d4f32de0859a224ca0
Bryan Silverthorn <bcs@cargo-cult.org>
2012-03-22 22:33:46 -0500

    Compile cython modules via setup.py.

--- setup.py
+++ setup.py
@@ -1,28 +1,34 @@
 import setuptools
 import setuptools.extension
-#import Cython.Distutils
 
-#extensions = [
-    #setuptools.extension.Extension(
-        #"za.embedded",
-        #[
-            #"za/embedded.pyx",
-            #"za/embedded/goertzel.c",
-            #"za/embedded/early_late.c",
-            #"za/embedded/fsk4_demapper.c",
-            #"za/embedded/fsk_demodulator.c",
-            #],
-        #extra_compile_args = ["-std=c99"],
-        #),
-    #]
+try:
+    import Cython.Distutils
+except ImportError:
+    cmdclass = {}
+    ext_modules = None
+else:
+    cmdclass = {"build_ext": Cython.Distutils.build_ext}
+    ext_modules = [
+        setuptools.extension.Extension("borg.bregman", ["src/python/borg/bregman.pyx"]),
+        setuptools.extension.Extension("borg.models", ["src/python/borg/models.pyx"]),
+        setuptools.extension.Extension("borg.planners", ["src/python/borg/planners.pyx"]),
+        setuptools.extension.Extension("borg.statistics", ["src/python/borg/statistics.pyx"]),
+        setuptools.extension.Extension("borg.domains.max_sat.features", ["src/python/borg/domains/max_sat/features.pyx"]),
+        setuptools.extension.Extension("borg.domains.max_sat.instance", ["src/python/borg/domains/max_sat/instance.pyx"]),
+        setuptools.extension.Extension("borg.domains.pb.features", ["src/python/borg/domains/pb/features.pyx"]),
+        setuptools.extension.Extension("borg.domains.pb.instance", ["src/python/borg/domains/pb/instance.pyx"]),
+        setuptools.extension.Extension("borg.domains.sat.features", ["src/python/borg/domains/sat/features.pyx"]),
+        setuptools.extension.Extension("borg.domains.sat.instance", ["src/python/borg/domains/sat/instance.pyx"]),
+        setuptools.extension.Extension("borg.test.test_statistics_c", ["src/python/borg/test/test_statistics_c.pyx"]),
+        ]
 
 setuptools.setup(
     name = "borg",
     version = "2012.4.01",
     packages = setuptools.find_packages("src/python"),
     package_dir = {"": "src/python"},
-    #cmdclass = {'build_ext': Cython.Distutils.build_ext},
-    #ext_modules = extensions,
+    cmdclass = cmdclass,
+    ext_modules = ext_modules,
     install_requires = [
         "Cython>=0.15.1",
         "numpy>=1.6.1",
